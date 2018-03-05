4d7138568bb1c3bff18b6e8f596442426897f325
cfree14 <cfree14@gmail.com>
2018-03-04 20:12:51 -0500

    complete doc editted

--- R/complete.R
+++ R/complete.R
@@ -4,7 +4,6 @@
 #' Checks the completeness of columns in a dataframe.
 #'
 #' @param data A dataframe
-#' @return A vector showing the the number of NA values in a dataframe
+#' @return A vector showing the number of NA values in each column of a dataframe
 #' @export
 complete <- function(data){apply(data, 2, function(x) sum(is.na(x)))}
-
