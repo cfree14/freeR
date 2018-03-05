eb0f1056699745fade58bb6edead5abafdc124f5
cfree14 <cfree14@gmail.com>
2018-03-05 08:42:52 -0500

    added formatted floor and ceiling functions

---
+++ R/ceiling1.R
@@ -0,0 +1,14 @@
+
+#' Round to ceiling of specified level
+#'
+#' Raises a number to the nearest 0.1, 0.5, 1, 5, 10, etc.
+#'
+#' @param x A number
+#' @return A rounded number
+#' @examples
+#' # Raise 0.34 to nearest 0.5
+#' ceiling1(0.34, level=0.5)
+#' #' Raise 35.6 to nearest 60
+#' ceiling1(35.6, level=50)
+#' @export
+ceiling1 <- function(x, level){ceiling(x/level)*level}
---
+++ R/floor1.R
@@ -0,0 +1,14 @@
+
+#' Round to floor of specified level
+#'
+#' Lowers a number to the nearest 0.1, 0.5, 1, 5, 10, etc.
+#'
+#' @param x A number
+#' @return A rounded number
+#' @examples
+#' # Lower 0.34 to nearest 0.25
+#' floor1(0.34, level=0.25)
+#' #' Raise 35.6 to nearest 30
+#' floor1(35.6, level=30)
+#' @export
+floor1 <- function(x, level){floor(x/level)*level}
