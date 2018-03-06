62560735d8c30fe83d143bd61213e94298f1898a
cfree14 <cfree14@gmail.com>
2018-03-06 07:54:54 -0500

    added examples

--- R/colorpal.R
+++ R/colorpal.R
@@ -6,5 +6,9 @@
 #' @param pal A vector of colors
 #' @param n Number of colors to be interpolated
 #' @return An expanded vector of colors
+#' @examples
+#' # Make 15 color palette from 11 color red-blue palette
+#' library(RColorBrewer)
+#' colorpal(brewer.pal(11, "RdBu"), 15)
 #' @export
 colorpal <- function(pal, n){colorRampPalette(pal)(n)}
