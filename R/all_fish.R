0e6446ee93f14b50e53987b1ee56d0306bb35aa1
Chris Free <cfree14@gmail.com>
2022-07-18 08:03:11 -0700

    fixed fishbase error

--- R/all_fish.R
+++ R/all_fish.R
@@ -12,7 +12,7 @@
 all_fish <- function(){
 
   # Build FB key
-  taxa_key_fb <- rfishbase::load_taxa(server="https://fishbase.ropensci.org") %>%
+  taxa_key_fb <- rfishbase::load_taxa(server="fishbase") %>% # server="https://fishbase.ropensci.org"
     as.data.frame() %>%
     mutate(type="fish") %>%
     select(type, everything()) %>%
@@ -22,7 +22,7 @@ all_fish <- function(){
     mutate_all(as.character)
 
   # Build SLB key
-  taxa_key_slb <- rfishbase::sealifebase %>%
+  taxa_key_slb <- rfishbase::load_taxa(server="sealifebase") %>% # rfishbase::sealifebase %>%
     as.data.frame() %>%
     mutate(type="invert") %>%
     select(type, everything()) %>%
