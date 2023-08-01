089d3a2be4ad46c2b4c2d7795424ec874f29520a
cfree14 <cfree14@gmail.com>
2023-08-01 12:38:53 -0700

    fixed fishlife to include new params

--- R/fishlife.R
+++ R/fishlife.R
@@ -15,32 +15,68 @@ fishlife <- function(species){
 
   # Setup container
   spp <- sort(unique(species))
-  fl <- data.frame(species=spp, linf_cm=NA, k=NA, winf_g=NA, tmax_yr=NA, tmat_yr=NA,
-                   m=NA, lmat_cm=NA, temp_c=NA, stringsAsFactors=F)
 
   # Loop through species
-  for(i in 1:nrow(fl)){
+  x <- spp[1]
+  fl_orig <- purrr::map_df(spp, function(x){
 
     # Get spp info
-    sciname <- fl$species[i]
+    sciname <- x
     genus <- stringr::word(sciname, 1)
     nwords_in_spp <- length(strsplit(sciname, " ")[[1]])
-    species <- stringr::word(sciname, start=2, end=nwords_in_spp)
-    species <- ifelse(species=="spp", "predictive", species)
+    species1 <- stringr::word(sciname, start=2, end=nwords_in_spp)
+    species1 <- ifelse(species1=="spp", "predictive", species1)
 
     # Try looking up in FishLife
-    spp_info <- try(FishLife::Plot_taxa(FishLife::Search_species(Genus=genus, Species=species)$match_taxonomy))
+    spp_info <- try(FishLife::Plot_taxa(FishLife::Search_species(Genus=genus, Species=species1)$match_taxonomy))
     if(inherits(spp_info, "try-error")){
       # Record blanks
-      fl[i,2:ncol(fl)] <- rep(NA, ncol(fl)-1)
+      spp_lh_vals_log <- rep(NA, 20)
+      names(spp_lh_vals_log)<-c("Loo", "K", "Winfinity", "tmax", "tm", "M", "Lm",
+                               "Temperature", "ln_var", "rho", "ln_MASPS", "ln_margsd", "h", "logitbound_h",
+                               "ln_Fmsy_over_M", "ln_Fmsy", "ln_r", "r", "ln_G", "G")
     }else{
       # Values are in log-scale except temperature
       spp_lh_vals_log <- spp_info[[1]]$Mean_pred
-      spp_lh_vals <- c(exp(spp_lh_vals_log[1:7]), spp_lh_vals_log[8])
-      fl[i,2:ncol(fl)] <- spp_lh_vals
     }
 
-  }
+    # Return
+    spp_lh_vals_log
+
+  })
+
+  # Format
+  fl <- fl_orig %>%
+    # Reduce
+    select(Loo, K, Winfinity, tmax, tm, M, Lm, Temperature, ln_var, rho, ln_MASPS, ln_margsd, h,
+           ln_Fmsy_over_M, ln_Fmsy, r, G) %>%
+    # Rename
+    rename(linf_cm=Loo,
+           k=K,
+           winf_g=Winfinity,
+           tmax_yr=tmax,
+           tmat_yr=tm,
+           m=M,
+           lmat_cm=Lm,
+           temp_c=Temperature,
+           ln_var=ln_var,
+           rho=rho,
+           masps=ln_MASPS,
+           margsd=ln_margsd,
+           h=h,
+           fmsy_div_m=ln_Fmsy_over_M,
+           fmsy=ln_Fmsy,
+           r=r,
+           g_yr=G) %>%
+    # Unlog
+    # r, G, FMSY/M, temperature are unlogged?
+    mutate_at(.vars=c("linf_cm", "k", "winf_g", "tmax_yr", "tmat_yr", "m", "lmat_cm",
+                      "ln_var", "masps", "margsd", "fmsy"), .funs=exp) %>%
+    # Add species
+    mutate(sci_name=spp) %>%
+    # Arrange
+    select(sci_name, everything())
+  fl
 
   # Return
   return(fl)
