9534bde9a412fddc5a910edbe1e708188115bae6
cfree14 <cfree14@gmail.com>
2023-08-01 15:23:54 -0700

    FishLife work

--- R/fishlife.R
+++ R/fishlife.R
@@ -51,31 +51,41 @@ fishlife <- function(species){
     select(Loo, K, Winfinity, tmax, tm, M, Lm, Temperature, ln_var, rho, ln_MASPS, ln_margsd, h,
            ln_Fmsy_over_M, ln_Fmsy, r, G) %>%
     # Rename
-    rename(linf_cm=Loo,
-           k=K,
-           winf_g=Winfinity,
-           tmax_yr=tmax,
-           tmat_yr=tm,
-           m=M,
-           lmat_cm=Lm,
-           temp_c=Temperature,
-           ln_var=ln_var,
-           rho=rho,
-           masps=ln_MASPS,
-           margsd=ln_margsd,
-           h=h,
-           fmsy_div_m=ln_Fmsy_over_M,
-           fmsy=ln_Fmsy,
-           r=r,
-           g_yr=G) %>%
-    # Unlog
+    rename(#Predicted
+           k=K,                       # Von B growth coefficient
+           linf_cm=Loo,               # Asymptotic length (cm)
+           winf_g=Winfinity,          # Asymptotic weight (g)
+           m=M,                       # Natural mortality (1/yr)
+           tmax_yr=tmax,              # Maximum age (yr)
+           tmat_yr=tm,                # Age at maturity (yr)
+           lmat_cm=Lm,                # Length at maturity (cm)
+           temp_c=Temperature,        # Temperature (°C)
+           recruit_sd_cond=ln_var,    # Conditional standard deviation of recruitment variability (τ)
+           recruit_sd_auto=rho,       # Autocorrelation of recruitment variability (ρ)
+           masps=ln_MASPS,            # Maximum annual spawners per spawner in excess of recruitment (r)
+           # Derived
+           recruit_sd_marg=ln_margsd, # Marginal standard deviation for recruitment (σ): σ = sqrt(τ^2 / (1-ρ^2))
+           h=h,                       # Steepness
+           fmsy_div_m=ln_Fmsy_over_M, # FMSY / M
+           fmsy=ln_Fmsy,              # FMSY
+           r=r,                       # Intrinsic growth rate
+           g_yr=G) %>%                # Generation time (yr)
+    # Exponentiate:
+    # Predicted: Temperature, recruitment autocorrelation, MASPS are unlogged
     # r, G, FMSY/M, temperature are unlogged?
-    mutate_at(.vars=c("linf_cm", "k", "winf_g", "tmax_yr", "tmat_yr", "m", "lmat_cm",
-                      "ln_var", "masps", "margsd", "fmsy"), .funs=exp) %>%
+    mutate_at(.vars=c("k", "linf_cm", "winf_g",
+                      "m",  "tmax_yr", "tmat_yr", "lmat_cm",
+                      "recruit_sd_cond",
+                      "recruit_sd_marg", "fmsy", "fmsy_div_m"), .funs=exp) %>%
     # Add species
     mutate(sci_name=spp) %>%
     # Arrange
-    select(sci_name, everything())
+    select(sci_name,
+           k, linf_cm, winf_g,
+           m, tmax_yr,  tmat_yr, lmat_cm,
+           recruit_sd_cond, recruit_sd_marg, recruit_sd_auto, masps, h,
+           r, g_yr, fmsy, fmsy_div_m,
+           everything())
   fl
 
   # Return
