a4a948469f78c9bbe34a5cf0f44932335ef6d34d
cfree14 <cfree14@gmail.com>
2025-11-19 09:30:26 -0800

    fixed all fish error

--- R/all_fish.R
+++ R/all_fish.R
@@ -12,27 +12,28 @@
 all_fish <- function(){
 
   # Build FB key
-  taxa_key_fb <- rfishbase::fishbase %>% # (1-old) rfishbase::load_taxa(server="fishbase") (2-old)# server="https://fishbase.ropensci.org"
+  taxa_key_fb <- rfishbase::load_taxa(server="fishbase") %>% # (1-old) rfishbase::fishbase (2-old)# server="https://fishbase.ropensci.org"
     as.data.frame() %>%
     mutate(type="fish") %>%
     select(type, everything()) %>%
     setNames(tolower(colnames(.))) %>%
-    mutate(sciname=paste(genus, species)) %>%
+    rename(sciname=species) %>%
     mutate_all(as.character)
 
   # Build SLB key
-  taxa_key_slb <- rfishbase::sealifebase %>% # (1-old) rfishbase::load_taxa(server="sealifebase") (2-old) rfishbase::sealifebase %>%
+  taxa_key_slb <- rfishbase::load_taxa(server="sealifebase") %>% # (1-old) rfishbase::sealifebase (2-old) rfishbase::sealifebase %>%
     as.data.frame() %>%
     mutate(type="invert") %>%
     select(type, everything()) %>%
     setNames(tolower(colnames(.))) %>%
-    mutate(sciname=paste(genus, species)) %>%
+    rename(sciname=species) %>%
     mutate_all(as.character)
 
+  # Merge FB and SLB keys
   taxa_key <-  taxa_key_fb %>%
     bind_rows(taxa_key_slb) %>%
     setNames(tolower(names(.))) %>%
-    select(type, class, order, family, genus, species, sciname) %>%
+    select(type, class, order, family, genus, sciname) %>%
     unique()
 
   return(taxa_key)
